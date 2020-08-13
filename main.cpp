e1d2fb0a19448416f87967a9bdf9f2cfcc12e2d5
caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2020-08-12 20:14:41 -0500

    remove old comments

--- main.cpp
+++ main.cpp
@@ -13,17 +13,14 @@ bool reduce(int start_position,string &input) {
   string substring = input.substr(start_position,2);
 
   if(substring == "rg" || substring == "gr"){
-    //return b
     input.replace(start_position,2,"b");
     retVal = true;
   }
   else if(substring == "rb" || substring == "br"){
-    //return g
      input.replace(start_position,2,"g");
      retVal = true;
   }
   else if(substring == "gb" || substring == "bg"){
-    //return r
     input.replace(start_position,2,"r");
     retVal = true;
   }
